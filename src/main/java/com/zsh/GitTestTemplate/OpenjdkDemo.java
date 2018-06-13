87a9b2de9d51107566498289207e0d777bae989a
ts-haozhang <ts-haozhang@hotmail.com>
2018-06-13 22:25:56 +0800

    add detail test

--- src/main/java/com/zsh/GitTestTemplate/OpenjdkDemo.java
+++ src/main/java/com/zsh/GitTestTemplate/OpenjdkDemo.java
@@ -3,6 +3,7 @@ package com.zsh.GitTestTemplate;
 public class OpenjdkDemo {
 	public static void main(String[] args) {
 		System.out.println("java openjdk come on");
+		System.out.println("234234234");
 	}
 
 }
