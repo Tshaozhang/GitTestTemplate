c70338ffd7e9ce7735bddc6e37879f4d47820d0d
ts-haozhang <ts-haozhang@hotmail.com>
2018-06-13 21:33:06 +0800

    add new file

---
+++ src/main/java/com/zsh/GitTestTemplate/App.java
@@ -0,0 +1,13 @@
+package com.zsh.GitTestTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
