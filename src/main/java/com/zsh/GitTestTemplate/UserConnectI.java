b15454bd5827197850a92c236c8b99d84e978bb7
ts-haozhang <ts-haozhang@hotmail.com>
2018-06-18 22:38:48 +0800

    还原代码

--- src/main/java/com/zsh/GitTestTemplate/UserConnectI.java
+++
@@ -1,8 +0,0 @@
-package com.zsh.GitTestTemplate;
-
-public interface UserConnectI {
-	
-	public String getUserInfo(String userId)throws Exception;
-	public String userRegister(String userName,String passwd,String phoneNum)throws Exception;
-
-}
