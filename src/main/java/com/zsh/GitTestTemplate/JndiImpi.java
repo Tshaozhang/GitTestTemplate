c23f1e8dbcd4149c059ffdc35e85fe8ad4ce562a
ts-haozhang <ts-haozhang@hotmail.com>
2018-06-18 22:01:25 +0800

    实现 jndi 接口

---
+++ src/main/java/com/zsh/GitTestTemplate/JndiImpi.java
@@ -0,0 +1,12 @@
+package com.zsh.GitTestTemplate;
+
+public class JndiImpi implements Jndi {
+
+	@Override
+	public void getUserStatus(String userId) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("已经收到验证");
+
+	}
+
+}
